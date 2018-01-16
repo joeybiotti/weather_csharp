23862710632b0f49d1ce4404a8e3e0b4614eeb18
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-16 14:37:29 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace csharp_weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Zip Code");
+            string zipCode = Console.ReadLine();
+
+        }
+    }
+}
