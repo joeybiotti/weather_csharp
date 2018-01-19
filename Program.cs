705d9a34e306ae8257cd2ee6890e6744a17d35e3
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-18 22:18:25 -0600

    added results class and serializer

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 
-namespace csharp_weather
+namespace WxAPP
 {
     class Program
     {
@@ -15,15 +17,19 @@ namespace csharp_weather
         private static async Task CheckLocalWeather()
         {
             var client = new HttpClient();
+            var serializer = new DataContractJsonSerializer(typeof(List<WxResults>));
 
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var stringTask = client.GetStringAsync("http://api.openweathermap.org/data/2.5/forecast?id=4644585&APPID=");
+            var stringTask = client.GetStringAsync("http://api.openweathermap.org/data/2.5/forecast?id="); //API KEY NEEDED
+            var streamTask = client.GetStreamAsync("http://api.openweathermap.org/data/2.5/forecast?id=4644585&APPID="); //API KEY NEEDED
 
-            var msg = await stringTask;
-            Console.WriteLine(msg);
+            var weatherResults = serializer.ReadObject(await streamTask) as List<WxResults>;
+
+            foreach(var result in weatherResults)
+                Console.WriteLine(result.List);
         }
     }
 }
